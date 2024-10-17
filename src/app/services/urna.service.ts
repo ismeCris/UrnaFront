607bcc977756b7f2e307ca7fb198b0a7b0e0c44c
Maczevski <maczevski@gmail.com>
2024-10-17 19:42:24 -0300

    services de eleitor e de voto

--- src/app/services/urna.service.ts
+++ src/app/services/urna.service.ts
@@ -1,9 +1,26 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Voto } from '../models/voto';
+import { Observable } from 'rxjs';
+import { Apuracao } from '../models/apuracao';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrnaService {
 
+  http = inject(HttpClient);
+
+  api = "http://localhost:8080/api/votos";
+
   constructor() { }
+
+  votar(voto : Voto, eleitorId : number) : Observable<string> {
+    return this.http.post<string>(this.api +`/votar/${eleitorId}`, {});
+  }
+
+  realizarApuração() : Observable<Apuracao>{
+    return this.http.get<Apuracao>(this.api + "/apuracao")    
+  }
+
 }
