291a166acbd0b52807b68095834bd923100cca7e
Maczevski <maczevski@gmail.com>
2024-10-17 21:21:33 -0300

    Navbar provisória

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,17 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/layout/login/login.component';
 import { PrincipalComponent } from './components/layout/principal/principal.component';
 import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
-import { UrnaComponent } from './components/layout/urna/urna.component';
+import { UrnaComponent } from './components/urna/urna.component';
 import { CandidatoListComponent } from './components/candidato/candidato-list/candidato-list.component';
 import { CandidatoFormComponent } from './components/candidato/candidato-form/candidato-form.component';
 import { EleitorListComponent } from './components/eleitor/eleitor-list/eleitor-list.component';
 import { EleitorFormComponent } from './components/eleitor/eleitor-form/eleitor-form.component';
+import { Apuracao } from './models/apuracao';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  {path: "urna", component: UrnaComponent},
+  {path: 'urna', component: UrnaComponent},
   { path: 'admin', component: PrincipalComponent, children: [
     { path: 'dashboard', component: DashboardComponent },
     { path: 'candidatos', component: CandidatoListComponent },
@@ -19,6 +20,7 @@ export const routes: Routes = [
     { path: 'candidatos/edit/:id', component: CandidatoFormComponent },
     { path: 'eleitores', component: EleitorListComponent },
     { path: 'eleitores/new', component: EleitorFormComponent },
-    { path: 'eleitores/edit/:id', component: EleitorFormComponent }
+    { path: 'eleitores/edit/:id', component: EleitorFormComponent },
+    { path: 'apuracao', component: Apuracao}
   ]}
 ];
--- src/app/models/eleitor.ts
+++ src/app/models/eleitor.ts
@@ -1,2 +1,18 @@
 export class Eleitor {
+  id!: number;
+  nomeCompleto!: string;
+  cpf!: string;
+  profissao!: string;
+  celular!: string;
+  telefone!: string;
+  email!: string;
+  status!: Status;
 }
+enum Status {
+  APTO = 'APTO',
+  INATIVO = 'INATIVO',
+  BLOQUEADO = 'BLOQUEADO',
+  PENDENTE = 'PENDENTE',
+  VOTOU = 'VOTOU',
+}
+
