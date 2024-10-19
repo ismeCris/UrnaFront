09de7342635519e7acd253b4670e7d5bbc72026d
ismycris <cristielythainara@gmail.com>
2024-10-19 13:51:20 -0300

    Atualiza candidato.service.ts

--- src/app/services/candidato.service.ts
+++ src/app/services/candidato.service.ts
@@ -1,9 +1,45 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Candidato } from '../models/candidato';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CandidatoService {
+  private http = inject(HttpClient);
+  private API = 'http://localhost:8080/api/candidatos';
 
   constructor() { }
+
+  findAll(): Observable<Candidato[]> {
+    return this.http.get<Candidato[]>(this.API+"/findAll");
+  }
+
+  findById(id: number): Observable<Candidato> {
+    return this.http.get<Candidato>(this.API+"/findById/"+id);
+  }
+
+  update(candidato: Candidato): Observable<string> {
+    return this.http.put<string>(this.API+"/update/"+candidato.id, candidato, {responseType: 'text' as 'json'});
+  }
+
+  save(candidato: Candidato): Observable<string> {
+    return this.http.post<string>(this.API+"/save",candidato, {responseType: 'text' as 'json'});
+  }
+
+  delete(id: number): Observable<string> {
+    return this.http.put<string>(this.API+"/delete/"+id, {responseType: 'text' as 'json'});
+  }
+
+  getPrefeitosAtivos(): Observable<Candidato[]> {
+    return this.http.get<Candidato[]>(this.API+"/prefeAtivos");
+  }
+
+  getVereadoresAtivos(): Observable<Candidato[]> {
+    return this.http.get<Candidato[]>(this.API+"/vereAtivos");
+  }
+  
+  
+  
 }
